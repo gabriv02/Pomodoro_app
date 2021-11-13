6528a5350d20c0596b84e4d195fd55d7af197ad5
Gabriela <a2020120887@estudiantes.upsa.edu.bo>
2021-11-13 02:06:53 -0400

    Add project files.

---
+++ Pomodoro_app/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomodoro_app.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pomodoro_app.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ Pomodoro_app/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pomodoro_app.Model
+{
+    public class Person
+    {
+        [Key]
+
+        public int PersonId { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = " El nombre debe tener entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre completo de la persona")]
+        public string Name { get; set; }
+        
+        [Display(Name = "Pasa tiempos")]
+        public string Hobbie { get; set; }
+    }
+}
